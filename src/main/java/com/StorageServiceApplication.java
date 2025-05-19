5feb8097fe58e37b3bbf36339f1c35cf8e294716
G/E/DIENSTEN_Jha Vipin Kumar <2273408@maruti.co.in>
2025-05-19 17:21:40 +0530

    Initial commit

---
+++ src/main/java/com/StorageServiceApplication.java
@@ -0,0 +1,63 @@
+package com;
+
+import com.service.StorageService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@OpenAPIDefinition(info = @Info(title = "File Storage", version = "1.0", description = "Spring Boot File Upload and Download REST API "))
+@SpringBootApplication
+@RestController
+@RequestMapping("/image")
+public class StorageServiceApplication {
+
+	@Autowired
+	private StorageService service;
+
+	@PostMapping
+	public ResponseEntity<?> uploadImage(@RequestParam("image")MultipartFile file) throws IOException {
+		String uploadImage = service.uploadImage(file);
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(uploadImage);
+	}
+
+	@GetMapping("/{fileName}")
+	public ResponseEntity<?> downloadImage(@PathVariable String fileName){
+		byte[] imageData=service.downloadImage(fileName);
+		return ResponseEntity.status(HttpStatus.OK)
+				.contentType(MediaType.valueOf("image/png"))
+				.body(imageData);
+
+	}
+
+
+	@PostMapping("/fileSystem")
+	public ResponseEntity<?> uploadImageToFIleSystem(@RequestParam("image")MultipartFile file) throws IOException {
+		String uploadImage = service.uploadImageToFileSystem(file);
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(uploadImage);
+	}
+
+	@GetMapping("/fileSystem/{fileName}")
+	public ResponseEntity<?> downloadImageFromFileSystem(@PathVariable String fileName) throws IOException {
+		byte[] imageData=service.downloadImageFromFileSystem(fileName);
+		return ResponseEntity.status(HttpStatus.OK)
+				.contentType(MediaType.valueOf("image/png"))
+				.body(imageData);
+
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageServiceApplication.class, args);
+	}
+
+}
